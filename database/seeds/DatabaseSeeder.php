0a35d7c89a69730c63c38bb0cd46379ad8cc39f7
lakatosgabor <lakatos.gabor998@gmail.com>
2020-07-25 12:20:12 +0200

    Első felhasználó felvitele

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Eloquent\Model; 
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        Eloquent::unguard();
+        $this->call('UsersTablesSeeder');
+        
     }
 }

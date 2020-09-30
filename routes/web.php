0a133fc78841b6f3254856ea6be253861f6568cb
lakatosgabor <lakatos.gabor998@gmail.com>
2020-09-30 17:02:43 +0200

    Rotue-ok beállítása

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,28 @@ Route::get('/', 'MainController@index');
 Route::get('/main', 'MainController@index');
 Route::post('/main/checklogin', 'MainController@checklogin');
 Route::get('main/dashboard', 'MainController@successlogin');
+Route::get('/tutorial', 'MainController@successlogin');
 Route::get('main/logout', 'MainController@logout');
 Route::get('dashboard','MenuController@dashboardmenu');
 Route::get('news','MenuController@newsmenu');
+//Route::get('tasks','MenuController@tasksmenu');
+Route::get('chat','MenuController@chatmenu');
+Route::get('warning','MenuController@warningmenu');
 
+Route::group(['middleware' => ['auth', 'oktato']], function() {
+    Route::get('/tutorial', function(){
+        return view('admin.tutorial');
+    });
+
+});
+
+
+Route::get('tasks', 'DocumentController@index');
+Route::post('/files', 'DocumentController@store');
+Route::get('/files', 'DocumentController@index');
+
+Route::get('files/{id}', 'DocumentController@show');
+
+
+
+Route::get('teszt', 'MenuController@tesztmenu');

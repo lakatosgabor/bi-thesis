500306ed25832325687fefe98d61061c946b639c
lakatosgabor <lakatos.gabor998@gmail.com>
2020-09-30 16:52:17 +0200

    Menü beállítások

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -5,15 +5,30 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Validator;
 use Auth;
+use App\Documents;
 
 class MenuController extends Controller{
 
     function dashboardmenu(){
-        return view('dashboard');
+        $file=Documents::all();
+        return view('dashboard', compact('file'));
     }
 
     function newsmenu(){
         return view('news');
     }
+
+    function tasksmenu(){
+        return view('tasks');
+    }
+
+    function chatmenu(){
+        return view('chat');
+    }
+
+    function warningmenu(){
+        return view('warning');
+    }
+
 }
 
